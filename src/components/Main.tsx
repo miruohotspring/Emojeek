2a7c73a68fdac39c34d28d52e49e8badf990999f
miruohotspring <miruohotspring@gmail.com>
2021-04-10 03:44:15 +0900

    各記事へのリアクション機能の追加 ToDo: リアクションの削除、現在のリアクションの表示、自分のリアクションのハイライト

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,8 @@ import { MyUser } from '../Interface';
 import { API, graphqlOperation } from 'aws-amplify'; 
 import { listReactionOnSpecificPost, listPostsSortedByCreatedAt } from '../graphql/queries';
 import GraphQLResult from 'aws-amplify';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 import {
   Button,
@@ -16,9 +18,15 @@ import {
   CircularProgress,
 } from '@material-ui/core';
 
+import { createReaction } from '../graphql/mutations';
+
 
 export function Main(props: MainProps): JSX.Element {
+    const initialReactionState: { [postId: string]: number } = {};
+    
     const [posts, setPosts] = useState({posts: []});
+    const [reactions, setReactions] = useState(initialReactionState);
+    const [pickerState, setPickerState] = useState<PickerState>({ show: false, postId: "", x: 0, y: 0 });
     
     const getPosts = async () => {
         const res: any = await API.graphql(graphqlOperation(listPostsSortedByCreatedAt, {
@@ -29,18 +37,28 @@ export function Main(props: MainProps): JSX.Element {
         setPosts({ posts: res.data.listPostsSortedByCreatedAt.items });
     }
     
+    async function getReaction(postId: string) {
+        const res: any = await API.graphql(graphqlOperation(listReactionOnSpecificPost, {
+            postId: postId
+        }));
+        console.log(res);
+    }
+    
     useEffect(() => {
         getPosts();
-        console.log(posts);
     }, []);
     
+    
     function showPost(post: any): JSX.Element {
+        const onReaction = (event: any) => {
+            setPickerState({ show: !pickerState.show, postId: post.id, x: event.clientX, y: event.clientY });
+        }
         return (
             <>
             <ListItem>
                 <List>
                 <ListItem>
-                <Typography>
+                <Typography variant="h4">
                     {post.title}
                 </Typography>
                 </ListItem>
@@ -56,13 +74,27 @@ export function Main(props: MainProps): JSX.Element {
                 </ListItem>
                 </List>
             </ListItem>
+            <Button onClick={onReaction}>+</Button>
             </>
         );
+    }
+    
+    const onPick = async(emoji: any) => {
+        const res = await API.graphql(graphqlOperation(createReaction, { input: {
+            postId: pickerState.postId,
+            emoji: emoji.native,
+        }}));
+        getReaction(pickerState.postId);
     }
     
     return (
         <>
             <Typography variant='h3'>Emojeek</Typography>
+            { pickerState.show && 
+                <div id="overlay" style={{top: pickerState.y, left: pickerState.x}}>
+                <Picker onSelect={(emoji: any) => {onPick(emoji)}} /> 
+                </div>
+            }
             <List>
             {posts.posts.map(post => (
                 showPost(post)
@@ -72,6 +104,16 @@ export function Main(props: MainProps): JSX.Element {
     );
 }
 
+interface Reaction {
+    count: number
+}
+
+interface PickerState {
+    show: boolean;
+    postId: string;
+    x: number;
+    y: number;
+}
 
 interface MainProps {
     user: MyUser | undefined;
