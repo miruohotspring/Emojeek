e61414d1a83138899f01fbe9913891d0753f8da0
miruohotspring <miruohotspring@gmail.com>
2022-05-03 01:38:19 +0900

    バックエンド環境を変更

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "emojeek": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "emojeek77f0a5d8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "emojeekGroupRole": "string"
+        }
+    }
+}
